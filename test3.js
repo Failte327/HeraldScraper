d80005db0212eb2b826f52810802f28d91e000dc
Failte327 <87578456+Failte327@users.noreply.github.com>
2021-10-13 10:03:04 -0600

    added more stat scrapes

--- test3.js
+++ test3.js
@@ -1,5 +1,6 @@
 const request = require("request-promise")
 const cheerio = require("cheerio");
+const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 
 request("https://herald.atlasfreeshard.com/playerstat.php?player_name=Villanelle", (error, response, html) => {
     if (!error && response.statusCode == 200) {
@@ -16,3 +17,53 @@ request("https://herald.atlasfreeshard.com/playerstat.php?player_name=Villanelle
 
 });
 
+await lib.discord.channels['@0.2.0'].messages.create({
+    "channel_id": `${context.params.event.channel_id}`,
+    "content": "",
+    "tts": false,
+    "embeds": [
+      {
+        "type": "rich",
+        "title": `Villanelle`,
+        "description": "",
+        "color": 0x3869ed,
+        "fields": [
+          {
+            "name": `Norseman Shadowblade`,
+            "value": `Guildless`,
+            "inline": true
+          },
+          {
+            "name": `Isen Vakten`,
+            "value": `2L2`
+          },
+          {
+            "name": `Realmpoints`,
+            "value": `14 330`,
+            "inline": true
+          },
+          {
+            "name": `Rank on the Server`,
+            "value": `88`
+          },
+          {
+            "name": `Rank in the Class`,
+            "value": `5`
+          },
+          {
+            "name": `Total Player Kills`,
+            "value": `175`
+          },
+          {
+            "name": `Total Solo Kills`,
+            "value": `21`
+          },
+          {
+              "name": `Total Deathblows`,
+              "value": `73`
+          }
+        ],
+        "url": `https://herald.atlasfreeshard.com/playerstat.php?player_name=Villanelle`
+      }
+    ]
+  });
